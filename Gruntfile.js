a193b42a52f6132262d002fc43494c17afa8dfe0
vogdb <vozhdb@gmail.com>
2013-08-31 22:24:08 +0400

    Create.

---
+++ Gruntfile.js
@@ -0,0 +1,35 @@
+module.exports = function (grunt) {
+
+  grunt.initConfig({
+
+    pkg: grunt.file.readJSON('package.json'),
+
+    banner: '/*!\n <%= pkg.title || pkg.name %> - v<%= pkg.version %> - '
+      + '<%= grunt.template.today("yyyy-mm-dd") %>\n'
+      + ' <%= (pkg.homepage ? "* " + pkg.homepage : pkg.repository.url) + "\\n" %>'
+      + ' Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %> <%= pkg.author.url %>;\n'
+      + ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %>\n'
+      + '\n Requires jQuery UI Autocomplete \n*/\n',
+
+    concat: {
+      options: {
+        banner: '<%= banner %>'
+      },
+      dist: {
+        src: ['src/ui.geo_autocomplete.js'],
+        dest: 'dist/<%= pkg.name %>.js'
+      }
+    },
+
+    watch: {
+      files: ['Gruntfile.js', 'src/**/*'],
+      tasks: 'concat'
+    }
+  })
+
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('default', ['concat']);
+
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# About
+This repository is the continuation of the [geo-autocomplete](http://code.google.com/p/geo-autocomplete/)
+library.
